477a6b7d75ccf99e9ed6c2ca3a58b5b84744c200
Matheus Eduardo Lopes de Carvalho <eduardomatheus360@gmail.com>
2025-07-22 20:37:55 -0300

    Add files via upload

---
+++ ex02.py
@@ -0,0 +1,6 @@
+n1 = 12
+n2 = 14
+
+soma = n1 + n2
+
+print("A soma dos dois valores é de ", soma)
---
+++ ex04.py
@@ -0,0 +1,7 @@
+produto = "cadeira infantil"
+preco = 12.4
+quantidade = 3 
+
+valorFinal = quantidade * preco
+
+print("O valor de 3 ", produto, "é de ", valorFinal)
